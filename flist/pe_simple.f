4535069947bd0dec56ee684a5c3925df3197452f
alan.zheng <69025555@qq.com>
2026-02-07 22:52:04 +0800

    Update flist for relative paths and add pe_simple.f
    - Update pe_top_design.f: Use relative paths (design/...)
    - Add pe_simple.f: Simplified filelist for simulation
    - Fix local_cache.v issue by excluding from simple flist
    
    Test: source setup.sh && iverilog -f flist/pe_simple.f -o /tmp/test && /tmp/test

---
+++ flist/pe_simple.f
@@ -0,0 +1,15 @@
+# Simple PE Design FileList for Simulation
+# ============================================
+# Paths relative to APACHE_HW_ROOT
+# ============================================
+
+# RTL Files (working modules only)
++incdir+design/pe_core/rtl
+design/pe_core/rtl/pe_top_simple.v
+design/pe_core/rtl/mac_array.v
+design/pe_core/rtl/activation_unit_simple.v
+design/pe_core/rtl/normalization_unit_simple.v
+design/pe_core/rtl/register_file.v
+
+# Testbench
+design/pe_core/rtl/tb_pe_core.v
--- flist/pe_top_design.f
+++ flist/pe_top_design.f
@@ -1,32 +1,33 @@
 # PE Top Complete Design FileList for Verdi
 # Generated for wave viewing and design analysis
 # ============================================
-# Note: Use relative paths (.) which resolves to APACHE_HW_ROOT
-# To setup: source setup.sh update-flist
-# To restore: source setup.sh restore-flist
+# Note: Paths are relative to APACHE_HW_ROOT
+# Usage: 
+#   cd $APACHE_HW_ROOT/flist
+#   iverilog -f pe_top_design.f
 # ============================================
 
 # PE Core RTL Files (Complete Design)
-+incdir+./design/pe_core/rtl
-./design/pe_core/rtl/pe_top_enhanced.v
-./design/pe_core/rtl/pe_core_complete.v
-./design/pe_core/rtl/mac_array.v
-./design/pe_core/rtl/activation_unit_simple.v
-./design/pe_core/rtl/normalization_unit_simple.v
-./design/pe_core/rtl/register_file.v
-./design/pe_core/rtl/local_cache.v
++incdir+design/pe_core/rtl
+design/pe_core/rtl/pe_top_enhanced.v
+design/pe_core/rtl/pe_core_complete.v
+design/pe_core/rtl/mac_array.v
+design/pe_core/rtl/activation_unit_simple.v
+design/pe_core/rtl/normalization_unit_simple.v
+design/pe_core/rtl/register_file.v
+design/pe_core/rtl/local_cache.v
 
 # Utility RTL Files (Clock and Sync Components)
-+incdir+./design/utils/rtl
-./design/utils/rtl/clock_buffer.v
-./design/utils/rtl/clock_divider.v
-./design/utils/rtl/clock_gating.v
-./design/utils/rtl/clock_mux.v
-./design/utils/rtl/clock_switch.v
-./design/utils/rtl/edge_detector.v
-./design/utils/rtl/sync_cell.v
++incdir+design/utils/rtl
+design/utils/rtl/clock_buffer.v
+design/utils/rtl/clock_divider.v
+design/utils/rtl/clock_gating.v
+design/utils/rtl/clock_mux.v
+design/utils/rtl/clock_switch.v
+design/utils/rtl/edge_detector.v
+design/utils/rtl/sync_cell.v
 
 # Testbench Files
-+incdir+./design/pe_core/rtl
-./design/pe_core/rtl/tb_pe_enhanced.v
-./design/pe_core/rtl/tb_pe_complete.v
++incdir+design/pe_core/rtl
+design/pe_core/rtl/tb_pe_enhanced.v
+design/pe_core/rtl/tb_pe_complete.v
